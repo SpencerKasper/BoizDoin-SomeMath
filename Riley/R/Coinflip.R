553b3d8ae74132e86e9b03a303d0074739612487
rileybusche <43003507+rileybusche@users.noreply.github.com>
2019-01-30 11:08:12 -0600

    Update Coinflip.R

--- Riley/R/Coinflip.R
+++ Riley/R/Coinflip.R
@@ -1,12 +1,40 @@
-#Coinflip 2.0
+#Coin Flip Probability 
 
+#Probability Function
 
-barValues <- rep(0,30)
-runs <- 100000
-for(i in 1:runs){
-  pos = sum(runif(30) < .5)
-  barValues[pos] <-  barValues[pos] + 1
+prob <- function(n,p,x){
+  #(n x) (p^x) ((1-p)^(n-x))
+  prob <- choose(n,x) * (p^x) * ((1-p)^(n-x))
+  return(prob)
 }
 
-barplot(barValues/runs)
+#Creating vector of all probabilites
+barValues <- prob(1000, .5, 0)
+for (i in 1:1000) {
+  barValues <- append(barValues, prob(1000, .5, i))
+}
 
+barValues
+barplot(barValues)
+sum(barValues[22:31])
+
+
+
+#Flipping 60 coins
+prob <- function(n,p,x){
+  #(n x) (p^x) ((1-p)^(n-x))
+  prob <- choose(n,x) * (p^x) * ((1-p)^(n-x))
+  return(prob)
+}
+
+#Storing the prob of 0-60 heads into probs
+probs <- prob(60, .5, 0)
+for (i in 1:60) {
+  probs <- append(probs, prob(60, .5, i))
+}
+
+#Prob that there are more than 34 heads
+sum(probs[36:61])
+
+#Prob that the number of heads is an odd number
+sum(probs[seq(2,61,2)])
